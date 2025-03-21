8b12a765d47789503a6b4e6d3ff396ef5f63f21e
Nurmukhanbet Sertay <nurmukhanbetsertay@gmail.com>
2025-03-21 06:34:50 +0500

    Initial Commit

---
+++ CombineProject/CombineProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineProjectApp.swift
+//  CombineProject
+//
+//  Created by Nurmukhanbet Sertay on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CombineProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineProject
+//
+//  Created by Nurmukhanbet Sertay on 21.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
